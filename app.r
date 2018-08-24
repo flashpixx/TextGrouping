a13b57fef01e409c5c921e79983208b731ee608c
Malte Aschermann <masc@tu-clausthal.de>
2018-08-24 15:43:35 +0200

    slider

--- app.r
+++ app.r
@@ -13,6 +13,9 @@ shinyApp(
             column(3,
                    wellPanel(
                         selectInput("language", label="Stopword Language", list("english", "danish", "german", "dutch", "finnish", "french", "hungarian", "italian", "norwegian", "portuguese", "russian", "spanish", "swedish"), selected = "english", multiple = FALSE, selectize = TRUE)
+                   ),
+                   wellPanel(
+                        sliderInput(inputId = "opt.cex", label = "Point Label Size", min = 0, max = 2, step = 0.25, value = 1)
                    )
             ),
             column(9,
@@ -42,8 +45,8 @@ shinyApp(
                     l_result <- build.neighbourhood( input$data, input$language )
                     #assign( "data", l_result, env = globalenv() )
 
-                    apcluster::plot( l_result$cluster, l_result$points, xaxt="n", yaxt="n" )
-                    text( l_result$points[,1], l_result$points[,2], l_result$labels, cex=1.6 )
+                    apcluster::plot( l_result$cluster, l_result$points, xaxt="n", yaxt="n", cex=input$opt.cex )
+                    text( l_result$points[,1], l_result$points[,2], l_result$labels, cex=input$opt.cex )
                 },
                 error = function(e) {
                     showNotification( paste(e), duration = 2 )
