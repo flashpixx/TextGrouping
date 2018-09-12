7513cb3a382bf66a3c3da2be17241e423503eff0
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2018-09-12 18:51:04 +0200

    make progress optional

--- analysis/somwine.r
+++ analysis/somwine.r
@@ -3,16 +3,16 @@
 # @param filename file name of appstore data
 # @see https://www.shanelynn.ie/self-organising-maps-for-customer-segmentation-using-r/
 # @see https://www.kaggle.com/uciml/red-wine-quality-cortez-et-al-2009
-som.wine <- function( input, progress )
+som.wine <- function( input, progress = NA )
 {
     # read data and convert to matrix
-    progress$set(message = "Converting data to matrix", value = 0.2)
+    if ( !is.na( progress ) ) progress$set(message = "Converting data to matrix", value = 0.2)
     l_data <- as.matrix( scale( readr::read_csv(input$winedata) ) )
 
     # create som and train
-    progress$set(message = "Creating SOM grid", value = 0.3)
+    if ( !is.na( progress ) ) progress$set(message = "Creating SOM grid", value = 0.3)
     l_grid <- kohonen::somgrid( xdim=input$opt.somdim, ydim=input$opt.somdim, topo="hexagonal" )
-    progress$set(message = "Training SOM", value = 0.4)
+    if ( !is.na( progress ) ) progress$set(message = "Training SOM", value = 0.4)
     l_som <- kohonen::som( l_data, grid = l_grid, rlen = input$opt.somiterations, alpha=c(0.05,0.01), keep.data = TRUE )
     
     return( l_som )
