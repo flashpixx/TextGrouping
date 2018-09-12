66a51d1ef062ae26e93462180138ef25e5b777e9
Malte Aschermann <masc@tu-clausthal.de>
2018-09-12 14:53:44 +0200

    fix indentation, progress

--- main.r
+++ main.r
@@ -13,13 +13,13 @@ source( "common/ui.r", local = TRUE )
 
 option_list = list(
   make_option( c("-p", "--port"), type="integer", default=8080, 
-              help="bind to port [default = %default]", metavar="integer" ),
+               help="bind to port [default = %default]", metavar="integer" ),
   make_option( "--shownodename", action="store_true", default=FALSE, 
-              help="display node name in title (helpful to distiguish app running in different docker containers) [default = %default]", metavar="character" ),
+               help="display node name in title (helpful to distiguish app running in different docker containers) [default = %default]", metavar="character" ),
   make_option( c("-s", "--somdim"), type="integer", default=35, 
-              help="som dimension [default = %default]", metavar="integer" ),
+               help="som dimension [default = %default]", metavar="integer" ),
   make_option( c("-i", "--somiterations"), type="integer", default=100, 
-              help="som iterations [default = %default]", metavar="integer" )
+               help="som iterations [default = %default]", metavar="integer" )
 ) 
 
 opt_parser = OptionParser( option_list = option_list )
@@ -28,55 +28,55 @@ opt = parse_args( opt_parser )
 quartzFonts(firasans = c("Fira Sans Book", "Fira Sans Bold", "Fira Sans Italic", "Fira Sans Bold Italic"))
 
 shiny::runApp(
-	shinyApp(
-	    ui = common.ui( opt ),
-	    server = function( input, output ) {
-	        observeEvent( input$viewtext, {
-	        	progress <- shiny::Progress$new(style = "notification")
-	            output$neighborhood <- renderPlot({
-	                tryCatch({
-	                	par(family='firasans')
-	                	progress$set(message = "Computing data", value = 0)
-	                    l_result <- build.neighbourhood( input$textdata, progress, input$language )
-	                    progress$set(message = "Rendering plots", value = 0.9)
-	                    apcluster::plot( l_result$cluster, l_result$points, xaxt="n", yaxt="n", cex=input$opt.cex )
-	                    text( l_result$points[,1], l_result$points[,2], l_result$labels, cex=input$opt.cex )
-	                    progress$set(message = "Finished", value = 1)
-	                },
-	                error = function(e) {
-	                    showNotification( paste(e), duration = 2 )
-	                })
-	            })
-	            progress$close()
-	        } )
-	        observeEvent( input$viewwine, {
-	        	progress <- shiny::Progress$new(style = "notification")
-	            tryCatch({
-	            	progress$set(message = "Computing data", value = 0)
-	                l_result <- som.wine( input, progress )
-	                progress$set(message = "Rendering plots", value = 0.8)
-	            	output$som <- renderPlot({
-	            		par( family='firasans' )
-	                    plot( l_result, type = "property", property = getCodes(l_result)[,3], main = "", palette.name = viridis::plasma, tricolor, heatkey = FALSE, shape = "straight", border = NA )
-	            	})
-	            	progress$set(message = "Rendering plots", value = 0.9)
-	                output$count <- renderPlot({
-	                	par( family='firasans' )
-	                	plot( l_result, type = "count", main="Node Count", palette.name = viridis::plasma, shape = "straight", border = NA )
-	                })
-	                output$changes <- renderPlot({
-	                	par( family='firasans' )
-	                	plot( l_result, type = "changes", main="Training Progress" )
-	                })
-	                progress$set(message = "Finished", value = 1) },
-	                error = function(e) {
-	                    showNotification( paste(e), duration = 2 )
-	            	})
-	            progress$close()
-	        	})
-	    }
-	),
-	host = "0.0.0.0",
-	port = as.numeric( opt$port )
+  shinyApp(
+    ui = common.ui( opt ),
+    server = function( input, output ) {
+      observeEvent( input$viewtext, {
+        output$neighborhood <- renderPlot({
+          tryCatch({
+            par(family='firasans')
+            progress <- shiny::Progress$new(style = "notification")
+            progress$set(message = "Building neighbourhood", value = 0.2)
+            l_result <- build.neighbourhood( input$textdata, input$language, progress )
+            progress$set(message = "Rendering plots", value = 0.7)
+            apcluster::plot( l_result$cluster, l_result$points, xaxt="n", yaxt="n", cex=input$opt.cex )
+            text( l_result$points[,1], l_result$points[,2], l_result$labels, cex=input$opt.cex )
+            progress$set(message = "Finished", value = 1)
+            progress$close()
+          },
+          error = function(e) {
+            showNotification( paste(e), duration = 2 )
+          })
+        })
+      })
+      observeEvent( input$viewwine, {
+        progress <- shiny::Progress$new(style = "notification")
+        tryCatch({
+          progress$set(message = "Computing data", value = 0)
+          l_result <- som.wine( input, progress )
+          progress$set(message = "Rendering plots", value = 0.8)
+          output$som <- renderPlot({
+            par( family='firasans' )
+            plot( l_result, type = "property", property = getCodes(l_result)[,3], main = "", palette.name = viridis::plasma, tricolor, heatkey = FALSE, shape = "straight", border = NA )
+          })
+          progress$set(message = "Rendering plots", value = 0.9)
+          output$count <- renderPlot({
+            par( family='firasans' )
+            plot( l_result, type = "count", main="Node Count", palette.name = viridis::plasma, shape = "straight", border = NA )
+          })
+          output$changes <- renderPlot({
+            par( family='firasans' )
+            plot( l_result, type = "changes", main="Training Progress" )
+          })
+          progress$set(message = "Finished", value = 1) },
+          error = function(e) {
+            showNotification( paste(e), duration = 2 )
+          })
+        progress$close()
+      })
+    }
+  ),
+  host = "0.0.0.0",
+  port = as.numeric( opt$port )
 )
 
