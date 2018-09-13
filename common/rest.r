ffdaa41f04cca03709aa66a574e0a5dfc90fe2db
Malte Aschermann <masc@tu-clausthal.de>
2018-09-13 10:34:55 +0200

    rest

--- common/rest.r
+++ common/rest.r
@@ -74,16 +74,49 @@ somPNG <- function( winedata, dimension=18, iterations=60 ) {
   plot( l_result, type = "changes", main="Training Progress" )
 }
 
+#* @post /som/jpeg
+#* @param winedata JSON encapsulated CVS
+#* @param dimension SOM dimensions for x and y
+#* @param iterations SOM iterations
+#* @jpeg (width = 1600, height = 1000)
+somJPEG <- function( winedata, dimension=18, iterations=60 ) {
+  par( family='firasans' )
+  par( fig=c(0, 0.7, 0, 1) )
+  l_result <- som.wine( gsub("\\\\n","\n", winedata ), dimension, iterations )
+  plot( l_result, type = "property", property = getCodes(l_result)[,3], main = "", palette.name = viridis::plasma, tricolor, heatkey = FALSE, shape = "straight", border = NA )
+  par( fig=c(0.7, 1, 0.5, 1), new=TRUE )
+  plot( l_result, type = "count", main="Node Count", palette.name = viridis::plasma, shape = "straight", border = NA )
+  par( fig=c(0.7, 1, 0.1, 0.5), new=TRUE )
+  plot( l_result, type = "changes", main="Training Progress" )
+}
+
 #* @post /som/pdf
 #* @param winedata JSON encapsulated CVS
 #* @serializer contentType list(type="application/pdf")
-somPDF <- function( miningtext ) {
+somPDF <- function( winedata, dimension=18, iterations=60 ) {
   tmp <- tempfile()
   pdf(tmp)
   par( family='Helvetica' )
-  l_result <- build.neighbourhood( gsub("\\\\n","\n", miningtext ) )
-  apcluster::plot( l_result$cluster, l_result$points, xaxt="n", yaxt="n", cex=0.6 )
-  text( l_result$points[,1], l_result$points[,2], l_result$labels, cex=0.6 )
+  par( fig=c(0, 0.7, 0, 1) )
+  l_result <- som.wine( gsub("\\\\n","\n", winedata ), dimension, iterations )
+  plot( l_result, type = "property", property = getCodes(l_result)[,3], main = "", palette.name = viridis::plasma, tricolor, heatkey = FALSE, shape = "straight", border = NA )
+  par( fig=c(0.7, 1, 0.5, 1), new=TRUE )
+  plot( l_result, type = "count", main="Node Count", palette.name = viridis::plasma, shape = "straight", border = NA )
+  par( fig=c(0.7, 1, 0.1, 0.5), new=TRUE )
+  plot( l_result, type = "changes", main="Training Progress" )
   dev.off()
   readBin(tmp, "raw", n=file.info(tmp)$size)
 }
+
+#* @post /som/json
+#* @param winedata JSON encapsulated CVS
+#* @html
+somJSON <- function( winedata, dimension=18, iterations=60 ) {
+  l_result <- som.wine( gsub("\\\\n","\n", winedata ), dimension, iterations )
+  paste0(
+    jsonlite::toJSON(data.frame(colnames(getCodes(l_result)),getCodes(l_result)), pretty = TRUE, unboxed = TRUE),
+    ",\n",
+    jsonlite::toJSON(list("training progress"=as.vector(l_result$changes)), pretty = TRUE, unboxed = TRUE)
+  )  
+}
+
